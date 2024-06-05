a04b4efb39d8f0f2ab92629f2deeec31cf15ca6f
tungkei <e0774490@u.nus.edu>
2024-06-05 16:21:52 +0800

    updated virtual env

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tongt\OneDrive\Documents\Kei\NUS\Mods\Y3S2\MHA\Flask\Test5\env
+set VIRTUAL_ENV=C:\Users\tongt\OneDrive\Documents\GitHub\Stitch-It\env
 
 if not defined PROMPT set PROMPT=$P$G
 
